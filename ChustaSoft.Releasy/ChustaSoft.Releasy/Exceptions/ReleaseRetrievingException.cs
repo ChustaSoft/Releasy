cec6b9ee6876d891922e1b4b5b39c7fbba3d8eca
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-04-07 00:51:31 +0200

    Organised objects inside main package

---
+++ ChustaSoft.Releasy/ChustaSoft.Releasy/Exceptions/ReleaseRetrievingException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChustaSoft.Releasy.Exceptions
+{
+    public class ReleaseRetrievingException : Exception
+    {
+
+        private const string ERROR_MESSAGE = "An error occurred while getting the project release notes";
+
+        public ReleaseRetrievingException(Exception innerException) 
+            : base(ERROR_MESSAGE, innerException)
+        { }
+    }
+}
