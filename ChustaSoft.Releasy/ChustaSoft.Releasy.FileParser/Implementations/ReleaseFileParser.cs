1eef1996b6c10e7771c505e6b92cdace7917b5bf
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-04-06 23:55:32 +0200

    Parsed all release info lines

--- ChustaSoft.Releasy/ChustaSoft.Releasy.FileParser/Implementations/ReleaseFileParser.cs
+++ ChustaSoft.Releasy/ChustaSoft.Releasy.FileParser/Implementations/ReleaseFileParser.cs
@@ -1,7 +1,9 @@
-﻿using ChustaSoft.Releasy.Configuration;
+﻿using ChustaSoft.Common.Helpers;
+using ChustaSoft.Releasy.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ChustaSoft.Releasy.FileParser
@@ -12,11 +14,15 @@ namespace ChustaSoft.Releasy.FileParser
         private const string STARTING_TEXT = "## [Releases]";
         private const string RELEASE_START_TEXT = "\n## ";
         private const string HEADER_INFO_SEPARATOR = " - ";
+        private const string CHANGES_BLOCK_SEPARATOR = "### ";
+        private const string CHANGETYPE_LINE_START = "\n- ";
         private const char NEW_LINE = '\n';
-        private const char RETURN = '\r';
         private const char HEADER_VERSION_START = '[';
         private const char HEADER_VERSION_END = ']';
         private const char EMPTY_CHARACTER = ' ';
+        private const int MIN_CHANGETYPE_LINE_LENGTH = 2;
+
+
         private readonly FileParserSettings _fileParserSettings;
 
 
@@ -58,6 +64,37 @@ namespace ChustaSoft.Releasy.FileParser
             var date = DateTime.Parse(lines[1].Trim());
 
             return (version, date);
+        }
+
+        internal IDictionary<ChangeType, IEnumerable<string>> GetChanges(string releaseText)
+        {
+            var changesBlocks = releaseText.Split(CHANGES_BLOCK_SEPARATOR);
+            var dictionary = new Dictionary<ChangeType, IEnumerable<string>>();
+
+            for (int i = 1; i < changesBlocks.Length; i++)
+            {
+                foreach (var type in EnumsHelper.GetEnumList<ChangeType>()) 
+                {
+                    if (changesBlocks[i].StartsWith(type.ToString()))
+                    {
+                        var lines = GetTypeLines(changesBlocks[i]).ToList();
+
+                        dictionary.Add(type, lines);
+                    }                        
+                }
+            }
+
+            return dictionary;
+        }
+
+
+        private IEnumerable<string> GetTypeLines(string typeText)
+        {
+            var allLines = typeText.Split(CHANGETYPE_LINE_START);
+
+            for (int i = 1; i < allLines.Length; i++)
+                if(allLines[i].Length > MIN_CHANGETYPE_LINE_LENGTH)
+                    yield return allLines[i];
         }
 
     }
