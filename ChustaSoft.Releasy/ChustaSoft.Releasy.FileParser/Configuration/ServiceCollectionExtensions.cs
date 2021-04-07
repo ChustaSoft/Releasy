44be7d3c2a9796d0ad8133d24637b14537bc9bea
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-04-07 11:44:34 +0200

    Implemented ReleaseService with cache handling

--- ChustaSoft.Releasy/ChustaSoft.Releasy.FileParser/Configuration/ServiceCollectionExtensions.cs
+++ ChustaSoft.Releasy/ChustaSoft.Releasy.FileParser/Configuration/ServiceCollectionExtensions.cs
@@ -6,13 +6,11 @@ namespace ChustaSoft.Releasy.Configuration
     public static class ServiceCollectionExtensions
     {
 
-        
-
-
         public static IReleasyConfigurationBuilder FromFile(this IReleasyConfigurationBuilder builder, string fileName = FileParserConstants.DEFAULT_CHANGELOG_FILENAME)
         {
             var settings = new FileParserSettings(fileName);
 
+            builder.Services.AddMemoryCache();
             builder.Services.AddSingleton(settings);
             builder.Services.AddTransient<IReleaseRepository, ReleaseFileParser>();
 
--- ChustaSoft.Releasy/ChustaSoft.Releasy/Implementations/ReleaseService.cs
+++ ChustaSoft.Releasy/ChustaSoft.Releasy/Implementations/ReleaseService.cs
@@ -1,29 +1,73 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ChustaSoft.Releasy
 {
     public class ReleaseService : IReleaseService
     {
-        public Task<ReleaseInfo> Get(string identifier)
+
+        private const string CACHE_KEY_DATA = "RELEASES";
+
+        private readonly IMemoryCache _cache;
+        private readonly IReleaseRepository _releaseRepository;
+
+
+        public ReleaseService(IMemoryCache cache, IReleaseRepository releaseRepository)
         {
-            throw new NotImplementedException();
+            _cache = cache;
+            _releaseRepository = releaseRepository;
         }
 
-        public Task<IEnumerable<ReleaseInfo>> GetAll()
+
+        public async Task<ReleaseInfo> Get(string identifier)
         {
-            throw new NotImplementedException();
+            var allReleases = await Load();
+
+            return allReleases.First(x => x.Identifier == identifier);
         }
 
-        public Task<IEnumerable<ReleaseInfo>> GetFrom(DateTime dateFrom)
+        public async Task<IEnumerable<ReleaseInfo>> GetAll()
         {
-            throw new NotImplementedException();
+            var allReleases = await Load();
+
+            return allReleases.OrderByDescending(x => x.Date);
         }
 
-        public Task<IEnumerable<ReleaseInfo>> GetFrom(string identifierFrom)
+        public async Task<IEnumerable<ReleaseInfo>> GetFrom(DateTime dateFrom)
         {
-            throw new NotImplementedException();
+            var allReleases = await Load();
+
+            return PerformGetFromDate(dateFrom, allReleases);
         }
+
+        public async Task<IEnumerable<ReleaseInfo>> GetFrom(string identifierFrom)
+        {
+            var allReleases = await Load();
+            var dateFrom = allReleases.First(x => x.Identifier == identifierFrom).Date;
+
+            return PerformGetFromDate(dateFrom, allReleases);
+        }
+
+
+        private async Task<IEnumerable<ReleaseInfo>> Load() 
+        {
+            var releases = Enumerable.Empty<ReleaseInfo>();
+            if (!_cache.TryGetValue(CACHE_KEY_DATA, out releases)) 
+            {
+                releases = await _releaseRepository.Load();
+
+                _cache.Set(CACHE_KEY_DATA, releases);
+            }
+            return releases;
+        }
+
+        private IEnumerable<ReleaseInfo> PerformGetFromDate(DateTime dateFrom, IEnumerable<ReleaseInfo> allReleases)
+        {
+            return allReleases.Where(x => x.Date >= dateFrom).OrderByDescending(x => x.Date);
+        }
+
     }
 }
